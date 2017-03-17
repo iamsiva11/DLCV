895f815f9d07bebea7156001512d5de7adf2bf89
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2017-03-17 11:57:43 +0530

    preprocess Image data/dataset

--- Pre-trained/image-tSNE-vgg16.py
+++ Pre-trained/image-tSNE-vgg16.py
@@ -4,9 +4,39 @@ for each image derived from the activations of the last fully-connected layer
 in a pre-trained convolutional neural network (convnet).
 """
 
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers import Convolution2D, ZeroPadding2D, MaxPooling2D
+from keras.optimizers import SGD
+
+from PIL import Image
+
+
+# vgg-16 Weigths file (~550MB)
 # https://drive.google.com/file/d/0Bz7KyqmuGsilT0J5dmRCM0ROVHc/view
 
+
 vgg_path = '../data/vgg16_weights.h5'
+
+
+def get_image(path):
+	"""convert the image to a numpy array of the correct size for further processing."""
+	img = Image.open(path)
+	if img.mode != 'RGB':
+        img = img.convert('RGB')
+    img = img.resize((224, 224), Image.ANTIALIAS)  # resize the image to fit into VGG-16
+    img = np.array(img.getdata(), np.uint8) # Convert to numpy array
+    img = img.reshape(224, 224, 3).astype(np.float32) # reshape and convert to float32
+    # subtract mean (probably unnecessary for t-SNE but good practice)
+    img[:,:,0] -= 123.68 
+    img[:,:,1] -= 116.779
+    img[:,:,2] -= 103.939
+    img = img.transpose((2,0,1))
+    img = np.expand_dims(img, axis=0)
+    return img
+
+
+
 
 
 def VGG_16(weights_path=None):
@@ -66,8 +96,6 @@ load the weights into it from the h5 file downloaded earlier.
     return model
 
 
-
 model = VGG_16(vgg_path)
 sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(optimizer=sgd, loss='categorical_crossentropy')
-
