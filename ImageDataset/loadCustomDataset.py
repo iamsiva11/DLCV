aef201e175fb6cf9653f7932e0b1d5b4fdfd3014
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2017-04-10 15:33:08 +0530

    create_dataset, fixed color issues

--- ImageDataset/loadCustomDataset.py
+++ ImageDataset/loadCustomDataset.py
@@ -17,8 +17,35 @@ def img_Preprocess(imDir):
 	    img_list.append(img)     
 	    return img_list
 
-def preprocess_image_new(image_path):
+def preprocess_image_1(image_path):
+	# Had color issues
     img = ndimage.imread(image_path)
     img = imresize(img, target_size) 
-    img = img.transpose((2,0,1))    
+    img = img.transpose((2,0,1))   
     return img	    
+
+def preprocess_image_2(image_path):
+	# Works fine
+    img = load_img(image_path, target_size=(img_height, img_width))
+    img = img_to_array(img)
+    return img    
+
+
+def create_dataset(imdir_class):    
+	"""
+	imdir_class - directory path of a class
+	"""
+	img_list=[]
+    imgs = sorted(os.listdir(imdir_class))
+    num = len(os.listdir(imdir_class))
+    for i in range(num):
+        img = imdir_class + os.sep + imgs[i]
+        x =  preprocess_image_2(img)
+        img_list.append(x)        
+    return img_list
+
+if __name__ == "__main__":
+	imDir_adidas ="path/data/"        
+	x_train = create_dataset(imDir_adidas)
+	x_train = np.stack(img_list)#.astype("float32")
+	x_train /= 255
